b9f48355eb51a5616a7abac2da3e649bb9ed1abc
mchandl1 <mchandl1@uvm.edu>
2018-05-10 17:16:55 -0600

    js includes lines

--- flash.js
+++ flash.js
@@ -6,3 +6,23 @@ svg = d3.select("#flash")
         .attr("width",width)
         .attr("height",height)
         .style("background-color","#000000")
+
+
+svg.on("mouseclick",function(){
+  x = d3.mouse(this)[0]
+  y = d3.mouse(this)[1]
+  var lines = d3.selectAll("line")
+                  .data([1,2,3,4,5,6,7,8,9,10])
+                  .enter()
+                  .append("line")
+                  .attr("x1",function(){
+                    return x
+                  })
+                  .attr("y1",function(){
+                    return y
+                  })
+                  .attr("x2",100)
+                  .attr("y2",150)
+                  .attr("stroke","yellow")
+                  .attr("stroke-width",.5)
+})
